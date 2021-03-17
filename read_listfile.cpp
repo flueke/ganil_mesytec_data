248804629ae742d64b6ab9930444169da86cfdf0
John Frankland <john.frankland@ganil.fr>
2021-03-17 14:49:26 +0100

    add example program to read mvme listfiles

---
+++ read_listfile.cpp
@@ -0,0 +1,146 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+
+#include "mesytec_data.h"
+#include "mesytec_module.h"
+
+int main(int, char* argv[])
+{
+   std::string filename = argv[1];
+   int nevents = std::stoi(std::string(argv[2]));
+
+   // define all modules in vme crate
+   auto mesytec_setup = mesytec::define_setup
+         (
+            {
+               {"MDPP-16", 0x0, 16, mesytec::SCP},
+               {"MDPP-32", 0x10, 32, mesytec::SCP}
+            }
+            );
+
+   int number_of_modules = mesytec_setup.size();
+
+   std::ifstream istrm(filename, std::ios::binary);
+   if (!istrm.is_open()) {
+      std::cout << "failed to open " << filename << '\n';
+      return 1;
+   }
+   else {
+      std::cout << "I opened file " << filename << std::endl;
+
+      // look for magic sequence
+      const std::vector<unsigned char> first_sequence {0x7d, 0xa, 0x20, 0x20, 0x20, 0x20, 0x7d, 0xa, 0x7d, 0xa};
+      const std::vector<unsigned char> second_sequence {0x2, 0x40, 0, 0xfa};
+      size_t sequence_size = first_sequence.size();
+      size_t sequence_found = 0;
+      uint64_t bytes_read=0;
+      while( sequence_found < sequence_size )
+      {
+         unsigned char bite;
+         istrm.read(reinterpret_cast<char*>(&bite), sizeof bite);
+         ++bytes_read;
+         if( bite == first_sequence[sequence_found] ) {
+            ++sequence_found;
+         }
+         else {
+            sequence_found = 0;
+         }
+      }
+      std::cout << "I found the first sequence after reading " << bytes_read << " bytes\n";
+      std::cout << "which is a multiple of 4 byte words? " << bytes_read%4 << " (if 0, true)\n";
+      sequence_size = second_sequence.size();
+      sequence_found = 0;
+      bytes_read=0;
+      while( sequence_found < sequence_size )
+      {
+         unsigned char bite;
+         istrm.read(reinterpret_cast<char*>(&bite), sizeof bite);
+         ++bytes_read;
+         if( bite == second_sequence[sequence_found] ) {
+            ++sequence_found;
+         }
+         else {
+            sequence_found = 0;
+         }
+      }
+      std::cout << "I found the second sequence after reading " << bytes_read << " bytes\n";
+      std::cout << "which is a multiple of 4 byte words? " << bytes_read%4 << " (if 0, true)\n";
+
+      // beginning of data stream ? nearly...
+
+      // need to read 2 consecutive EOE to find start of data (EOE - EOE - HEADER..)
+      int number_eoe=0;
+      for(int words=0; words<50; ++words){
+         uint32_t DATA = mesytec::read_data_word(istrm);
+         if(mesytec::is_end_of_event(DATA)) ++number_eoe;
+         if(number_eoe==2) break;
+      }
+      // next word should be header
+      auto next_word = mesytec::read_data_word(istrm);
+      if(!mesytec::is_header(next_word))
+      {
+         printf("Fatal error. next word should be header\n");
+         throw(std::runtime_error("data corruption"));
+      }
+
+      std::map<uint32_t,mesytec::mdpp_event> event_map;
+
+      while(nevents--){
+         // next_word is header...
+         auto words_to_read = mesytec::length_of_data(next_word);
+         mesytec::mdpp_module_data mod_data{mesytec::module_id(next_word), words_to_read};
+         auto& module = mesytec_setup[mod_data.module_id];
+
+         // (words_to_read-1) data words + 1 EOE
+         bool got_eoe=false;
+         while(words_to_read--)
+         {
+            next_word = mesytec::read_data_word(istrm);
+            if(mesytec::is_mdpp_data(next_word)) {
+               module.set_data_word(next_word);
+               //module.print_mdpp_data();
+               mod_data.add_data( module.data_type(), module.channel_number(), module.channel_data());
+            }
+            else if(mesytec::is_end_of_event(next_word))
+            {
+               if(words_to_read){
+                  // too early - missing data
+                  printf("Fatal error. EOE read before end of data: words_to_read=%d\n", words_to_read);
+                  throw(std::runtime_error("incomplete data buffer"));
+               }
+               else {
+                  got_eoe=true;
+                  //MTEC.print_eoe(next_word);
+                  mod_data.event_counter = mesytec::event_counter(next_word);
+                  mesytec::mdpp_event& event = event_map[mod_data.event_counter];
+                  event.event_counter = mod_data.event_counter;
+                  event.add_module_data(mod_data);
+                  if(event.is_full(number_of_modules))
+                  {
+                     // event is complete, can be sent to buffer etc.
+                     event.ls();
+                     // delete event after sending/printing
+                     auto it = event_map.find(event.event_counter);
+                     event_map.erase(it);
+                  }
+               }
+            }
+         }
+         // should only exit after reading EOE
+         if(!got_eoe)
+         {
+            printf("Fatal error. never read EOE in data stream\n");
+            throw(std::runtime_error("data corruption"));
+         }
+         // now read to next header
+         bool got_header=false;
+         do{
+            next_word = mesytec::read_data_word(istrm);
+            got_header=mesytec::is_header(next_word);
+         } while(!got_header);
+      }
+   }
+}
