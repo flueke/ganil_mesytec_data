603d8e728dae9ca66ce7ced19802ca14a3e979a7
John Frankland <john.frankland@ganil.fr>
2023-03-30 11:50:53 +0200

    fix bug in parser (hopefully)
     apparently valid data read from modules with unknown id could be added
     to events, but without any module header (because unknown)

--- lib/mesytec_buffer_reader.h
+++ lib/mesytec_buffer_reader.h
@@ -389,24 +389,11 @@ namespace mesytec
 
          total_number_events_parsed = 0;
 
-//         uint16_t expected_data_words(0),actual_data_words(0);
-
          bool unknown_module = false;
 
          while(bytes_left_in_buffer)
          {
             auto next_word = read_data_word(buf_pos);
-//            if( is_fill_word(next_word) )
-//            {
-//               // fill word (=0) to keep even number of words
-
-//               // fill words are included in the number of words stored in the module header
-//               // therefore they must be subtracted from the expected number of data words
-//               if(expected_data_words) --expected_data_words;
-//            }
-#ifdef DEBUG
-            std::cout << std::hex << std::showbase << next_word << " : ";
-#endif
             if(unknown_module){
                std::cout << std::hex << std::showbase << next_word;
                if(is_vmmr_adc_data(next_word))
@@ -431,44 +418,11 @@ namespace mesytec
                // check if we just read the EOE for the module
                if(is_end_of_event(next_word))
                {
-#ifdef DEBUG
-                  std::cout << "EOE :";
-#endif
-                  if(!mesytec_setup.readout.dummy_module())// not a dummy module, i.e. 'START' or 'END' markers
+                  if(!mesytec_setup.readout.dummy_module() && !unknown_module)// not a dummy module, i.e. 'START' or 'END' markers
                   {
-                     // did we read expected amount of data for module?
-//                     if(!unknown_module && actual_data_words!=expected_data_words)
-//                     {
-//                        std::cerr << "*** Read wrong number of data words for module id " << std::hex << std::showbase << (int)mod_data.get_module_id() << std::endl;
-//                        std::cerr << "    Expected = " << std::dec << expected_data_words << "   Actual = " << actual_data_words << std::endl;
-//                        mod_data.ls(mesytec_setup);
-
-//                        auto header_pos = buf_pos-4;
-//                        auto old_word = read_data_word(header_pos);
-//                        while(old_word != mod_data.get_header_word())
-//                        {
-//                           header_pos-=4; old_word = read_data_word(header_pos);
-//                        }
-//                        while(header_pos!=buf_pos+4)
-//                        {
-//                           old_word = read_data_word(header_pos);
-//                           std::cerr << std::hex << std::showbase << old_word <<std::endl;
-//                           header_pos+=4;
-//                        }
-//                        //assert(actual_data_words==expected_data_words);
-//                     }
-//                     actual_data_words=expected_data_words=0;
-
                      if(mod_data.has_data()) {
                         mesy_event.add_module_data(mod_data);
-#ifdef DEBUG
-                        std::cout << " added data to event";
-#endif
                      }
-#ifdef DEBUG
-                     else
-                        std::cout << " no data added to event";
-#endif
                   }
 
                   unknown_module=false;
@@ -476,38 +430,20 @@ namespace mesytec
                   mesytec_setup.readout.module_end_of_event();
                   if(mesytec_setup.readout.readout_complete())
                   {
-#ifdef DEBUG
-                     std::cout << " readout complete : treat_complete_event : ";
-#endif
                      treat_complete_event(F);
                   }
-#ifdef DEBUG
-                  else
-                     std::cout << std::endl;
-#endif
                }
-               else if(!mesytec_setup.readout.dummy_module())
+               else if(!mesytec_setup.readout.dummy_module() && !unknown_module)
                {
                   // read data from VME module ?
                   if(is_mesytec_data(next_word))
                   {
-#ifdef DEBUG
-                     std::cout << "DATA: "; std::cout << mesytec_setup.get_module(mod_data.get_module_id()).decode_data(next_word) << std::endl;
-#endif
                      mod_data.add_data(next_word);
-//                     ++actual_data_words;
                   }
-#ifdef DEBUG
-                  else
-                     std::cout << decode_type(next_word) << std::endl;
-#endif
                }
             }
             else if(is_module_header(next_word))
             {
-#ifdef DEBUG
-               std::cout << "HEADER : id = " << std::hex << std::showbase << (int) module_id(next_word);
-#endif
                got_header = mesytec_setup.readout.is_next_module(module_id(next_word));
 
                if(got_header){
@@ -515,17 +451,8 @@ namespace mesytec
                   {
                      // not a dummy module, i.e. 'START' or 'END' markers
                      auto firmware = mesytec_setup.get_module(module_id(next_word)).firmware;
-#ifdef DEBUG
-                     std::cout << " : real\n";
-#endif
                      mod_data.set_header_word(next_word,firmware);
-//                     expected_data_words = mod_data.get_number_of_data_words();
-//                     actual_data_words=0;
                   }
-#ifdef DEBUG
-                  else
-                     std::cout << " : dummy\n";
-#endif
                }
                else if(mesytec_setup.readout.in_readout_cycle())
                {
@@ -536,10 +463,6 @@ namespace mesytec
                   std::cout << std::hex << std::showbase << next_word << std::endl;
                }
             }
-#ifdef DEBUG
-            else
-               std::cout << decode_type(next_word) << std::endl;
-#endif
             // advance to next 32-bit word in buffer
             buf_pos+=4;
             bytes_left_in_buffer-=4;
